2e2ff2bcebc81b20c18ecf1db8b663fd7390d4ff
Gaurav Atreya <allmanpride@gmail.com>
2024-10-17 23:23:43 -0400

    Ability to call Rust native/plugin functions from python

--- nadi-py/src/functions.rs
+++ nadi-py/src/functions.rs
@@ -1,6 +1,9 @@
-use crate::node::PyNode;
-use nadi_core::functions::NadiFunctions;
-use nadi_core::network::Network;
+use crate::{attrs::AttrValue, network::PyNetwork, node::PyNode};
+use nadi_core::abi_stable::std_types::{RResult, RString, RVec};
+use nadi_core::functions::FunctionCtx;
+use nadi_core::functions::{NadiFunctions, NetworkFunctionBox, NodeFunctionBox};
+use nadi_core::prelude::*;
+use std::collections::HashMap;
 
 use pyo3::{
     exceptions::{PyAttributeError, PyKeyError},
@@ -15,6 +18,41 @@ impl PyNadiFunctions {
     #[new]
     fn new() -> Self {
         Self(NadiFunctions::new())
+    }
+
+    #[pyo3(signature = (function, nodes, *args, **kwargs))]
+    fn nodes(
+        &self,
+        function: &str,
+        nodes: Vec<PyNode>,
+        args: Vec<AttrValue>,
+        kwargs: Option<HashMap<String, AttrValue>>,
+    ) -> PyResult<()> {
+        let args: Vec<Attribute> = args.into_iter().map(|v| v.into()).collect();
+        let kwargs = kwargs
+            .map(|kw| kw.into_iter().map(|(k, v)| (k.into(), v.into())).collect())
+            .unwrap_or_default();
+        let ctx = FunctionCtx::from_arg_kwarg(args, kwargs);
+        let nodes: RVec<Node> = nodes.into_iter().map(|n| n.0).collect();
+        self.0.call_node(function, nodes.as_rslice(), &ctx)?;
+        Ok(())
+    }
+
+    #[pyo3(signature = (function, network, *args, **kwargs))]
+    fn network(
+        &self,
+        function: &str,
+        mut network: PyNetwork,
+        args: Vec<AttrValue>,
+        kwargs: Option<HashMap<String, AttrValue>>,
+    ) -> PyResult<()> {
+        let args: Vec<Attribute> = args.into_iter().map(|v| v.into()).collect();
+        let kwargs = kwargs
+            .map(|kw| kw.into_iter().map(|(k, v)| (k.into(), v.into())).collect())
+            .unwrap_or_default();
+        let ctx = FunctionCtx::from_arg_kwarg(args, kwargs);
+        self.0.call_network(function, &mut network.0, &ctx)?;
+        Ok(())
     }
 
     // todo register python functions into nadi/node functions
