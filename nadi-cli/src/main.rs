a176223ddba29b88d9d76b9f748733102916f78c
Gaurav Atreya <allmanpride@gmail.com>
2025-01-16 22:11:58 -0500

    Update for nadi version 0.5.0

--- nadi-cli/src/main.rs
+++ nadi-cli/src/main.rs
@@ -1,5 +1,5 @@
-use std::{io::Read, path::PathBuf};
-
+use std::{io::Read, path::{PathBuf, Path}};
+use nadi_core::parser::NadiError;
 use clap::{Parser, ValueEnum};
 use nadi_core::{functions::NadiFunctions, network::Network};
 
@@ -39,8 +39,11 @@ struct CliArgs {
     task: Option<String>,
     /// Tasks file to run; if `--stdin` is also provided runs this before stdin
     tasks: Option<PathBuf>,
+    /// Show the tasks file, do not do anything
+    #[arg(short, long, action, requires="tasks")]
+    show: bool,
     /// Use stdin for the tasks; reads the whole stdin before execution
-    #[arg(short, long, action)]
+    #[arg(short='S', long, action)]
     stdin: bool,
 }
 
@@ -48,7 +51,9 @@ fn main() -> anyhow::Result<()> {
     let args = CliArgs::parse();
     let functions = NadiFunctions::new();
 
-    if let Some(dir) = args.generate_doc {
+    if args.show {
+	show_tasks(&args.tasks.unwrap());
+    } else if let Some(dir) = args.generate_doc {
         functions.plugins_doc(&dir)?;
     } else if let Some(func) = args.fnhelp {
         println!("{}", functions.help(&func).unwrap_or_default());
@@ -86,26 +91,54 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn show_tasks(filename: &Path) {
+    let txt = std::fs::read_to_string(filename).unwrap();
+    match nadi_core::parser::tokenizer::get_tokens(&txt){
+        Ok(tokens) => {
+	    println!("\n----File Tokens----");
+            for tok in &tokens {
+                tok.colored_print();
+            }
+	    match nadi_core::parser::tasks::parse(tokens) {
+		Ok(tasks) => {
+		    println!("\n----Parsed Tasks----");
+		    for task in tasks {
+			println!("{}", task.to_colored_string());
+		    }
+		}
+		Err(e) => println!("{}", e.user_msg(Some(&filename.to_string_lossy()))),
+	    };
+	},
+        Err(e) => println!("{}", e.user_msg(Some(&filename.to_string_lossy()))),
+    }
+}
+
 fn execute_tasks(functions: &NadiFunctions, txt: &str, args: &CliArgs) -> anyhow::Result<()> {
-    let script =
-        nadi_core::parser::functions::parse_script_complete(&txt).map_err(anyhow::Error::msg)?;
+    let tokens = nadi_core::parser::tokenizer::get_tokens(&txt)?;
+    let tasks = match nadi_core::parser::tasks::parse(tokens) {
+	Ok(t) => t,
+	Err(e) => return Err(anyhow::Error::msg(e.user_msg(None))),
+    };
     if args.print_tasks {
-        for fc in &script {
+        for fc in &tasks {
             println!("{}", fc.to_colored_string());
         }
     }
 
-    let mut net = if let Some(ref net) = args.network {
-        Network::from_file(net)?
+    let net = if let Some(ref net) = args.network {
+        Some(Network::from_file(net)?)
     } else {
-        // if network not given start with empty network
-        Network::default()
+        None
     };
-    eprintln!("** Running {} Script **", script.len());
-    for fc in &script {
-        functions
-            .execute(fc, &mut net)
-            .map_err(anyhow::Error::msg)?;
+    eprintln!("** Running {} Script **", tasks.len());
+
+    let mut tasks_ctx = nadi_core::tasks::TaskContext::new(net);
+    for fc in tasks {
+        match tasks_ctx.execute(fc) {
+	    Ok(Some(p)) => println!("{p}"),
+	    Err(p) => return Err(anyhow::Error::msg(p)),
+	    _ => (),
+	}
     }
     Ok(())
 }
