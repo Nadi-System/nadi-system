24659f64ad5d4465499f60ce19a404a7d40f9762
Gaurav Atreya <allmanpride@gmail.com>
2025-07-24 17:10:42 -0400

    Deactivate the tasks syntax checker for now due to performace issues

--- nadi-ide/src/editor.rs
+++ nadi-ide/src/editor.rs
@@ -554,7 +554,8 @@ impl Editor {
     pub fn subscription(&self) -> Subscription<Message> {
         Subscription::batch([
             time::every(Duration::from_millis(100)).map(|_| Message::MaybeSaveEditHist),
-            time::every(Duration::from_secs(1)).map(|_| Message::MaybeParseTasks),
+            // Takes too much processing power, and makes everything slow
+            // time::every(Duration::from_secs(1)).map(|_| Message::MaybeParseTasks),
         ])
     }
 }
